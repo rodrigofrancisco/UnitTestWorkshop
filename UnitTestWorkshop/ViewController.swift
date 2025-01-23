035ccbfc5e6a06488ba1c989f8752c5f2d8bb419
Rodrigo Francisco <rfranciscop@liverpool.com.mx>
2025-01-23 16:04:50 -0600

    Initial Commit

---
+++ UnitTestWorkshop/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  UnitTestWorkshop
+//
+//  Created by RODRIGO FRANCISCO PABLO on 23/01/25.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
