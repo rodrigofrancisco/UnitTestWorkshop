8a41d5d15533ea1b6ee4bc13dbf3b84e1f288091
Rodrigo Francisco <rfranciscop@liverpool.com.mx>
2025-02-24 09:27:07 -0600

    Add test setup for orders service

--- UnitTestWorkshopTests/Example3/OrderServiceTests.swift
+++ UnitTestWorkshopTests/Example3/OrderServiceTests.swift
@@ -6,30 +6,25 @@
 //
 
 import XCTest
+@testable import UnitTestWorkshop
 
 final class OrderServiceTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func test_getDetails_successfullyGetsData() {
+        let client = APIClient(session: .shared)
+        let sut = OrderService(client: client)
+        
+        let expectation = expectation(description: "Expecting orders")
+        
+        sut.getDetails(from: "https://order-service.com", parameters: [:]) { result in
+            switch result {
+            case .success(let success):
+                XCTAssertEqual(success.status.code, 0)
+            case .failure:
+                XCTFail("Did not retrieve data")
+            }
+            expectation.fulfill()
         }
+        
+        wait(for: [expectation], timeout: 1)
     }
-
 }
