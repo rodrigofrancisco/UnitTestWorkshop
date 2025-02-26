45b70e3b5fc25f779b88370438cac3f2105c6c40
Rodrigo Francisco <rfranciscop@liverpool.com.mx>
2025-02-26 08:23:01 -0600

    Add another example for testing services

---
+++ UnitTestWorkshop/Example5/CancellationService.swift
@@ -0,0 +1,39 @@
+//
+//  CancellationService.swift
+//  UnitTestWorkshop
+//
+//  Created by RODRIGO FRANCISCO PABLO on 26/02/25.
+//
+
+protocol HTTPPostClient {
+    func post<T: Encodable, U: Decodable>(endpoint: String, body: T, headers: [String : Any]?, completion: @escaping (Result<U, Error>) -> Void)
+}
+
+struct CanceItemsModel: Encodable {
+    struct Item: Encodable {
+        let quantity: Int
+        let commerceItemID, skuID: String
+    }
+
+    let items: [Item]
+    let shippingGroupID, orderID, returnReasonID: String
+    let cancelItemsCount: Int
+}
+
+struct CancellationResponse: Decodable {
+    let cancelAuthorizationNo, noPedido, boleta, refundReturnCode: String
+    let refundMessage: String
+}
+
+struct CancellationService {
+    private let client: HTTPPostClient
+    
+    
+    init(client: HTTPPostClient) {
+        self.client = client
+    }
+    
+    func cancelItems(using endpoint: String, body: CanceItemsModel, completion: @Sendable @escaping (Result<CancellationResponse, Error>) -> Void) {
+        client.post(endpoint: endpoint, body: body, headers: nil, completion: completion)
+    }
+}
