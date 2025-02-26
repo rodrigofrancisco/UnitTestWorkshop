adb434570299d6aa2580b80b1b41f8e2b6de44f7
Rodrigo Francisco <rfranciscop@liverpool.com.mx>
2025-02-26 08:44:31 -0600

    Add unit test template

---
+++ UnitTestWorkshopTests/Example5/CancellationServiceTests.swift
@@ -0,0 +1,15 @@
+//
+//  CancellationServiceTests.swift
+//  UnitTestWorkshopTests
+//
+//  Created by RODRIGO FRANCISCO PABLO on 26/02/25.
+//
+
+import XCTest
+@testable import UnitTestWorkshop
+
+final class CancellationServiceTests: XCTestCase {
+    func test_cancelItems_succeedsWhenAddingRightParams() {
+        let sut = CancellationService()
+    }
+}
